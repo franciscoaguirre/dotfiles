0d56858a03255392e91b385afa8db0a47746b077
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2026-01-02 12:20:37 -0300

    fix: wrong key for claude shortcut

--- home/programs.nix
+++ home/programs.nix
@@ -105,7 +105,9 @@
       keys.normal = {
         esc = ":w";
         "C-y" = ":sh zellij run -n Yazi -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- bash ~/.config/helix/yazi-picker.sh open %{buffer_name}";
-        space = ":sh zellij run -c -d right -- claude";
+        space = {
+          c = ":sh zellij run -c -d right -- claude";
+        };
       };
     };
   };
